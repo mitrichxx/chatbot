c29b2128c6cfc11be65fd36a0cac0fa4b1060372
ikvel <n.a.karasev@hotmail.com>
2020-09-29 18:51:49 +0300

    fail controller

---
+++ src/main/java/com/wjuh/chatbot/State.java
@@ -0,0 +1,6 @@
+package com.wjuh.chatbot;
+
+public enum State {
+    START,
+
+}
--- src/main/java/com/wjuh/chatbot/controller/HelloController.java
+++ src/main/java/com/wjuh/chatbot/controller/HelloController.java
@@ -2,10 +2,7 @@ package com.wjuh.chatbot.controller;
 
 import com.wjuh.chatbot.VjuhBot;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
