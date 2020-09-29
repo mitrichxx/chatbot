3c4c1de15dd428d67c624f745f0492d27cd30dd6
mitrich <mitrichxx@gmail.com>
2020-09-29 13:52:28 +0300

    ChatBot: первый тест

--- src/main/java/com/wjuh/chatbot/ChatbotApplication.java
+++ src/main/java/com/wjuh/chatbot/ChatbotApplication.java
@@ -2,12 +2,22 @@ package com.wjuh.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class ChatbotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatbotApplication.class, args);
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new VjuhBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
 }
