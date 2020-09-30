f18e76f98e943b212f2c0ef39c2f441cfcfd198c
azskit <azskit@mail.ru>
2020-09-30 09:00:41 +0300

    Update TestStuxnetMessage.java

--- src/main/java/com/wjuh/chatbot/message/test/TestStuxnetMessage.java
+++ src/main/java/com/wjuh/chatbot/message/test/TestStuxnetMessage.java
@@ -9,9 +9,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TestStuxnetMessage extends TestMessage{
-    public static final String ANSWER_1 = "Промышленный вирус";
-    public static final String ANSWER_2 = "Троянская программа";
-    public static final String ANSWER_3 = "Макровирус";
+    public static final String ANSWER_1 = "Да,это мой знакомый,я ему доверяю";
+    public static final String ANSWER_2 = "Нет,продиктую только номер карты,этого достаточно для перевода";
+    public static final String ANSWER_3 = "Повешу трубку";
     public static final List<String> ANSWERS = Arrays.asList(ANSWER_1, ANSWER_2, ANSWER_3);
     public TestStuxnetMessage(User user, Chat chat, String[] arguments) {
         super(user, chat, arguments);
@@ -19,7 +19,7 @@ public class TestStuxnetMessage extends TestMessage{
 
     @Override
     public String getTag() {
-        return "Stuxnet это ...";
+        return "Вам звонит знакомый и говорит.что готов вернуть вам долг,но для этого ему нужно знать номер вашей карты, cvv-код и код из смс. Сообщители вы ему эти данные?";
     }
 
     @Override
